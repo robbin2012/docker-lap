ac35bc6fa11582c2b83a38a3325ba84bb7bf0b42
robbin2012 <robbin.joe@gmail.com>
2016-09-01 10:37:56 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#run
+
+# Run Apache:
+if [ $LOG_LEVEL == 'debug' ]; then
+    /usr/sbin/apachectl -DFOREGROUND -k start -e debug
+else
+    &>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
+fi
